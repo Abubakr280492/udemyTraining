0726cdd355f83b87389cddb80c12855ac7e24ec3
abubakr280492 <a.abdullaev3@student.inha.uz>
2019-07-26 15:29:47 +0500

    02_numeric

---
+++ 02_numeric/main.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+func main() {
+    fmt.Printf("%d - %b \n", 38, 40)
+    fmt.Printf("%d - %b -%x \n", 28, 28, 28)
+    fmt.Printf("%d - %b -%x \n", 16 , 16,16)
+    fmt.Printf("%d - %b -%x \n", 10 , 10,10)
+    fmt.Printf("%d - %b -%x \n", 5 , 5,5)
+    fmt.Printf("%d - %b -%x \n", 42 , 42,42)
+    fmt.Printf("%d - %b -%#x \n", 42 , 42,42)
+
+
+//    fmt.Printf("%d  decimal  - %binry -%hexidecimal \n", 5 , 5,5)
+//
+
+    
+}
