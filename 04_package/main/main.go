71f743c15fa588d333501c770204ea5357458c58
abubakr280492 <a.abdullaev3@student.inha.uz>
2019-07-29 14:10:51 +0500

    04_package

--- 04_package/main/main.go
+++ 04_package/main/main.go
@@ -1,9 +1,9 @@
 package main
 import (
-	 //"fmt"
-   //  "https://github.com/Abubakr280492/udemyTraining/tree/master/04_package"
+	 "fmt"
+     "github.com/Abubakr280492/udemyTraining/tree/master/04_package/stringutil"
 )	
 func main()  {
-	//fmt.Println(stringutil.Reverse("!oG ibdA")) //use  Reverse package
-	//fmt.Println(stringutil.MyName)  // Use this variable   to be Visible Capitaliza  myname
+	fmt.Println(stringutil.Reverse("!oG ibdA")) //use  Reverse package
+	fmt.Println(stringutil.MyName)  // Use this variable   to be Visible Capitaliza  myname
 }
