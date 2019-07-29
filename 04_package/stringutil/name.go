e16e904bdaf54163e1cae3b19ddf22d883d84bb9
abubakr280492 <a.abdullaev3@student.inha.uz>
2019-07-29 14:06:15 +0500

    04_package

---
+++ 04_package/stringutil/name.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main() {
+    for i :=0; i <200; i++ {
+    fmt.Printf("%d - %b -%x \n",i ,i,i)
+	}
+
+	for i :=0; i <200; i++ {
+		fmt.Printf("%d \t %b \t %x \n",i ,i,i)
+		}
+
+//    fmt.Printf("%d  decimal  - %binry -%hexidecimal \n", 5 , 5,5)
+//
+
+    
+}
