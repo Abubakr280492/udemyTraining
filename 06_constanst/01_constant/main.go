33be78ef7f5ce2b7ebe0a981c27f410829650d64
abubakr280492 <a.abdullaev3@student.inha.uz>
2019-07-31 14:46:22 +0500

    constants

---
+++ 06_constanst/01_constant/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+const p string = "Death and taxes"
+const q = 44
+
+func main()  {
+	const q = 42 
+
+	fmt.Println("p - ", p)
+	fmt.Println("q - ", q)
+}
---
+++ 06_constanst/02_Multiple_initialization /main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+
+const (
+	A = iota //0
+	B = iota //1
+	c = iota //2
+	PI = 3.14
+	Language = "GO"
+	d = iota //5
+    f = iota //6 
+)
+
+const (
+	q = iota //0   another constant it resets and statrs again from zero 
+	w = iota
+	e = iota
+
+)
+
+
+const(
+	 _ = iota    // 0
+	 r = iota*10   //1*10
+	 t = iota*10   //2*10
+)
+
+const (
+	_ = iota
+	KB = 1<<(iota*10)
+	MB = 1<<(iota*10)
+	GB = 1<<(iota*10)
+	TB = 1<<(iota*10)
+
+)
+func main()  {
+	fmt.Println(PI)
+	fmt.Println(Language)
+	fmt.Println(A)
+	fmt.Println(B)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(f)
+	
+	
+	fmt.Println(q)
+	fmt.Println(w)
+	fmt.Println(e)
+
+	fmt.Println(r)
+	fmt.Println(t)
+
+	fmt.Println("binary\t\tdecimal")
+	fmt.Printf("%b\t",KB)
+	fmt.Printf("%d\n",KB)
+	fmt.Printf("%b\t",MB)
+	fmt.Printf("%d\n",MB)
+	fmt.Printf("%b\t",GB)
+	fmt.Printf("%d\n",GB)
+	fmt.Printf("%b\t",TB)
+	fmt.Printf("%d\n",TB)
+
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
+	fmt.Println("Toshkentda soat", time.Now())
+
+
+}
