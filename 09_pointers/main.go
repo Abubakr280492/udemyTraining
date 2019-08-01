466b5f809418bf709baabb8ddcc83b362ff4c2e9
abubakr280492 <a.abdullaev3@student.inha.uz>
2019-08-01 12:12:39 +0500

    Crash course

--- 09_pointers/main.go
+++ 09_pointers/main.go
@@ -6,13 +6,13 @@ func main()  {
 	x := 13%3
 
 	fmt.Println(x)
-	if x==1 {
+	if x==1 {      // dont do x = 1  
 		fmt.Println("Odd")
 	}else {
 		fmt.Println("Even")
 	}
 
-	for i:=0; i<100; i++ {
+	for i:=0; i<100; i++ {  //don't forget simicolon 
 		if i%2==1{
 			fmt.Println("Odd")
 		}else{
